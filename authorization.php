dfc9d1ab19c78076e7af574a4d960e4da6e464d1
atrush <atrush.unit@gmail.com>
2017-08-29 16:01:04 +0300

    push

---
+++ authorization.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>authorization</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+    <link href="./css/reset.css" rel="stylesheet" type="text/css">
+    <link href="./css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<input  type="text" placeholder="Login" name="log">
+<input  type="password" placeholder="Password" name="pass">
+
+<input  type="text" placeholder="Email" name="mail">
+<input  type="text" placeholder="Confirm password" name="confirm_pass">
+
+<?php require_once "./blocks/footer.php" ?>
+</body>
+</html>
